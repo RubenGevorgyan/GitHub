d72c2ef60143fa0deb97a998611aae71b23e40a9
RubenGevorgyan <38076066+RubenGevorgyan@users.noreply.github.com>
2019-06-03 00:14:54 +0400

    Only JS and alligning footer left

--- React Project/Index.js
+++ React Project/Index.js
@@ -1,1 +1,13 @@
-Index.js
+window.onload = function() {
+  var animation = document.getElementById("animation")
+  animation.style.marginLeft = "400px";
+};
+function Homefunction(){
+	alert("Home was clicked");
+}
+function Aboutfunction(){
+	alert("About was clicked");
+}
+function Contactsfunction(){
+	alert("Contacts was clicked");
+}
